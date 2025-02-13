336d069a4da10c55673aa31d5e507656a862ab52
3CXD <martinezibarracesar10@gmail.com>
2025-02-12 22:09:31 -0600

    Modo difícil implementado

--- script.js
+++ script.js
@@ -11,19 +11,59 @@ let firstCard = false;
 let secondCard = false;
 
 //Items array
-const items = [
-  { name: "bee", image: "bee.png" },
-  { name: "crocodile", image: "crocodile.png" },
-  { name: "macaw", image: "macaw.png" },
-  { name: "gorilla", image: "gorilla.png" },
-  { name: "tiger", image: "tiger.png" },
-  { name: "monkey", image: "monkey.png" },
-  { name: "chameleon", image: "chameleon.png" },
-  { name: "piranha", image: "piranha.png" },
-  { name: "anaconda", image: "anaconda.png" },
-  { name: "sloth", image: "sloth.png" },
-  { name: "cockatoo", image: "cockatoo.png" },
-  { name: "toucan", image: "toucan.png" },
+const items1 = [
+  { name: "Ate", image: "images/Ate.png" },
+  { name: "Ate", image: "images/Eight.png" },
+  { name: "Bare", image: "images/Bare.png" },
+  { name: "Bare", image: "images/Bear.png" },
+  { name: "Brake", image: "images/Brake.png" },
+  { name: "Brake", image: "images/Break.png" },
+  { name: "Flavor", image: "images/Flavor.png" },
+  { name: "Flavor", image: "images/Flower.png" },
+  { name: "Hole", image: "images/Hole.png" },
+  { name: "Hole", image: "images/Whole.png" },
+  { name: "Mail", image: "images/Mail.png" },
+  { name: "Mail", image: "images/Male.png" },
+  { name: "Meat", image: "images/Meat.png" },
+  { name: "Meat", image: "images/Meet.png" },
+  { name: "Pair", image: "images/Pair.png" },
+  { name: "Pair", image: "images/Pear.png" },
+];
+let items2 = [
+  { name: "Plain", image: "images/Plain.png" },
+  { name: "Plain", image: "images/Plane.png" },
+  { name: "Sea", image: "images/Sea.png" },
+  { name: "Sea", image: "images/See.png" },
+  { name: "Blew", image: "images/Blew.jpg" },
+  { name: "Blew", image: "images/Blue.jpg" },
+  { name: "Knew", image: "images/Knew.png" },
+  { name: "Knew", image: "images/New.jpg" },
+  { name: "weak", image: "images/Weak.jpg" },
+  { name: "weak", image: "images/Week.jpg" },
+  { name: "Know", image: "images/Know.jpg" },
+  { name: "Know", image: "images/No.jpg" },
+  { name: "Toe", image: "images/Toe.jpg" },
+  { name: "Toe", image: "images/Tow.jpg" },
+  { name: "Wood", image: "images/Wood.png" },
+  { name: "Wood", image: "images/Would.png" },
+];
+let items3 = [
+  { name: "Cell", image: "images/Cell.avif" },
+  { name: "Cell", image: "images/Sell.jpg" },
+  { name: "Hour", image: "images/Hour.jpg" },
+  { name: "Hour", image: "images/Ours.jpg" },
+  { name: "Knight", image: "images/Knight.jpg" },
+  { name: "Knight", image: "images/Night.jpg" },
+  { name: "One", image: "images/One.jpg" },
+  { name: "One", image: "images/Won.jpg" },
+  { name: "Peace", image: "images/Peace.jpg" },
+  { name: "Peace", image: "images/Piece.jpg" },
+  { name: "Right", image: "images/Right.jpeg" },
+  { name: "Right", image: "images/Write.webp" },
+  { name: "Son", image: "images/Son.jpeg" },
+  { name: "Son", image: "images/Sun.webp" },
+  { name: "Steal", image: "images/Steal.jpg" },
+  { name: "Steal", image: "images/Steel.webp" },
 ];
 
 //Initial Time
@@ -53,36 +93,31 @@ const movesCounter = () => {
   moves.innerHTML = `<span>Moves:</span>${movesCount}`;
 };
 
-//Pick random objects from the items array
+//Pick random objects from the items1 array
 const generateRandom = (size = 4) => {
-  //temporary array
-  let tempArray = [...items];
-  //initializes cardValues array
-  let cardValues = [];
-  //size should be double (4*4 matrix)/2 since pairs of objects would exist
-  size = (size * size) / 2;
-  //Random object selection
-  for (let i = 0; i < size; i++) {
-    const randomIndex = Math.floor(Math.random() * tempArray.length);
-    cardValues.push(tempArray[randomIndex]);
-    //once selected remove the object from temp array
-    tempArray.splice(randomIndex, 1);
+  let randomValue = Math.floor(Math.random() * 3); // Genera un número entre 0 y 2
+  switch (randomValue) {
+    case 0:
+      return [...items1];
+      break;
+    case 1:
+      return [...items2];
+      break;
+    case 2:
+      return [...items3];
+      break;
+    default:
+      console.log("Error: Valor fuera de rango");
   }
-  return cardValues;
+  return [...items2];
 };
 
 const matrixGenerator = (cardValues, size = 4) => {
   gameContainer.innerHTML = "";
-  cardValues = [...cardValues, ...cardValues];
-  //simple shuffle
+  // Mezclar los valores sin duplicarlos
   cardValues.sort(() => Math.random() - 0.5);
+  
   for (let i = 0; i < size * size; i++) {
-    /*
-        Create Cards
-        before => front side (contains question mark)
-        after => back side (contains actual image);
-        data-card-values is a custom attribute which stores the names of the cards to match later
-      */
     gameContainer.innerHTML += `
      <div class="card-container" data-card-value="${cardValues[i].name}">
         <div class="card-before">?</div>
@@ -91,50 +126,37 @@ const matrixGenerator = (cardValues, size = 4) => {
      </div>
      `;
   }
-  //Grid
+
   gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
 
-  //Cards
   cards = document.querySelectorAll(".card-container");
   cards.forEach((card) => {
     card.addEventListener("click", () => {
-      //If selected card is not matched yet then only run (i.e already matched card when clicked would be ignored)
       if (!card.classList.contains("matched")) {
-        //flip the cliked card
         card.classList.add("flipped");
-        //if it is the firstcard (!firstCard since firstCard is initially false)
         if (!firstCard) {
-          //so current card will become firstCard
           firstCard = card;
-          //current cards value becomes firstCardValue
           firstCardValue = card.getAttribute("data-card-value");
         } else {
-          //increment moves since user selected second card
           movesCounter();
-          //secondCard and value
           secondCard = card;
           let secondCardValue = card.getAttribute("data-card-value");
+
           if (firstCardValue == secondCardValue) {
-            //if both cards match add matched class so these cards would beignored next time
             firstCard.classList.add("matched");
             secondCard.classList.add("matched");
-            //set firstCard to false since next card would be first now
             firstCard = false;
-            //winCount increment as user found a correct match
             winCount += 1;
-            //check if winCount ==half of cardValues
             if (winCount == Math.floor(cardValues.length / 2)) {
               result.innerHTML = `<h2>You Won</h2>
             <h4>Moves: ${movesCount}</h4>`;
               stopGame();
             }
           } else {
-            //if the cards dont match
-            //flip the cards back to normal
             let [tempFirst, tempSecond] = [firstCard, secondCard];
             firstCard = false;
             secondCard = false;
-            let delay = setTimeout(() => {
+            setTimeout(() => {
               tempFirst.classList.remove("flipped");
               tempSecond.classList.remove("flipped");
             }, 900);
